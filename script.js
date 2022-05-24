d02c4ef1188dc8acd52eb75e2018d29c1e4f9582
Brian1.66 <105819990+Br1an166@users.noreply.github.com>
2022-05-24 20:40:28 +0000

    kinda broke it but ill i it tomorrow (which is 5/25/22)

--- script.js
+++ script.js
@@ -1,37 +1,82 @@
 function ChangeColor(selection) {
   const box = document.getElementById(selection);
-  const resultBox = document.getElementById("b0");
+  const resultBox = document.getElementById("guess1");
   let color = box.innerHTML;
   console.log(color);
   resultBox.style.backgroundColor = color;
 
 }
-let el = document.getElementById("b1");
+  function ChangeColor(selection) {
+  const box = document.getElementById(selection);
+  const resultBox = document.getElementById("guess2");
+  let color = box.innerHTML;
+  console.log(color);
+  resultBox.style.backgroundColor = color;
+  }
+//const secretWord = ["White", "Black" , "Red" , "Blue" , "Green" , "Yellow"];  
+//let randomColor = getRandom();
+  
+
+let el = document.getElementById("b1 1");
 el.addEventListener("click", function() {
-  ChangeColor("b1");
+  ChangeColor("b1 1");
 }, false);
 
-el = document.getElementById("b2");
+el = document.getElementById("b2 1");
 el.addEventListener("click", function() {
-  ChangeColor("b2");
+  ChangeColor("b2 1");
 }, false);
 
-el = document.getElementById("b3");
+el = document.getElementById("b3 1");
 el.addEventListener("click", function() {
-  ChangeColor("b3");
+  ChangeColor("b3 1");
 }, false);
 
-el = document.getElementById("b4");
+el = document.getElementById("b4 1");
 el.addEventListener("click", function() {
-  ChangeColor("b4");
+  ChangeColor("b4 1");
 }, false);
 
-el = document.getElementById("b5");
+el = document.getElementById("b5 1");
 el.addEventListener("click", function() {
-  ChangeColor("b5");
+  ChangeColor("b5 1");
 }, false);
 
-el = document.getElementById("b6");
+el = document.getElementById("b6 1");
 el.addEventListener("click", function() {
-  ChangeColor("b6");
+  ChangeColor("b6 1");
 }, false);
+
+
+
+
+el = document.getElementById("b1 2");
+el.addEventListener("click", function() {
+  ChangeColor("b1 2");
+}, false);
+
+el = document.getElementById("b2 2");
+el.addEventListener("click", function() {
+  ChangeColor("b2 2");
+}, false);
+
+el = document.getElementById("b3 2");
+el.addEventListener("click", function() {
+  ChangeColor("b3 2");
+}, false);
+
+el = document.getElementById("b4 2");
+el.addEventListener("click", function() {
+  ChangeColor("b4 2");
+}, false);
+
+el = document.getElementById("b5 2");
+el.addEventListener("click", function() {
+  ChangeColor("b5 2");
+}, false);
+
+el = document.getElementById("b6 2");
+el.addEventListener("click", function() {
+  ChangeColor("b6 2");
+}, false);
+
