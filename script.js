dc274665db0f9e723ebd99722eb649d16167f112
Brian1.66 <105819990+Br1an166@users.noreply.github.com>
2022-06-01 19:54:54 +0000

    went back to a previous save and fixed it

--- script.js
+++ script.js
@@ -1,82 +1,37 @@
 function ChangeColor(selection) {
   const box = document.getElementById(selection);
-  const resultBox = document.getElementById("guess1");
+  const resultBox = document.getElementById("b0");
   let color = box.innerHTML;
   console.log(color);
   resultBox.style.backgroundColor = color;
 
 }
-  function ChangeColor(selection) {
-  const box = document.getElementById(selection);
-  const resultBox = document.getElementById("guess2");
-  let color = box.innerHTML;
-  console.log(color);
-  resultBox.style.backgroundColor = color;
-  }
-//const secretWord = ["White", "Black" , "Red" , "Blue" , "Green" , "Yellow"];  
-//let randomColor = getRandom();
-  
-
-let el = document.getElementById("b1 1");
+let el = document.getElementById("b1");
 el.addEventListener("click", function() {
-  ChangeColor("b1 1");
+  ChangeColor("b1");
 }, false);
 
-el = document.getElementById("b2 1");
+el = document.getElementById("b2");
 el.addEventListener("click", function() {
-  ChangeColor("b2 1");
+  ChangeColor("b2");
 }, false);
 
-el = document.getElementById("b3 1");
+el = document.getElementById("b3");
 el.addEventListener("click", function() {
-  ChangeColor("b3 1");
+  ChangeColor("b3");
 }, false);
 
-el = document.getElementById("b4 1");
+el = document.getElementById("b4");
 el.addEventListener("click", function() {
-  ChangeColor("b4 1");
+  ChangeColor("b4");
 }, false);
 
-el = document.getElementById("b5 1");
+el = document.getElementById("b5");
 el.addEventListener("click", function() {
-  ChangeColor("b5 1");
+  ChangeColor("b5");
 }, false);
 
-el = document.getElementById("b6 1");
+el = document.getElementById("b6");
 el.addEventListener("click", function() {
-  ChangeColor("b6 1");
+  ChangeColor("b6");
 }, false);
-
-
-
-
-el = document.getElementById("b1 2");
-el.addEventListener("click", function() {
-  ChangeColor("b1 2");
-}, false);
-
-el = document.getElementById("b2 2");
-el.addEventListener("click", function() {
-  ChangeColor("b2 2");
-}, false);
-
-el = document.getElementById("b3 2");
-el.addEventListener("click", function() {
-  ChangeColor("b3 2");
-}, false);
-
-el = document.getElementById("b4 2");
-el.addEventListener("click", function() {
-  ChangeColor("b4 2");
-}, false);
-
-el = document.getElementById("b5 2");
-el.addEventListener("click", function() {
-  ChangeColor("b5 2");
-}, false);
-
-el = document.getElementById("b6 2");
-el.addEventListener("click", function() {
-  ChangeColor("b6 2");
-}, false);
-
