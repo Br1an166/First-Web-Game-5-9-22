b91d186cc52ea8033114207068de1da2d6eb66b1
Brian1.66 <105819990+Br1an166@users.noreply.github.com>
2022-06-07 20:44:11 +0000

    Big advancement with the video tutorial

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
 document.addEventListener('DOMContentLoaded'), () => {
-  
-}}
+
+const cardArray = [
+  {
+    name: 'Red',
+    img: 'Images/Red1.png'
+  },
+{
+    name: 'Blue',
+    img: 'Images/Blue1.png'
+  },
+
+{
+    name: 'Yellow',
+    img: 'Images/Yellow1.png'
+  },
+
+{
+    name: 'Green',
+    img: 'Images/Green1.png'
+  },
+
+{
+    name: 'Orange',
+    img: 'Images/Orange1.png'
+  },
+
+{
+    name: 'Purple',
+    img: 'Images/Purple1.png'
+  },
+]
+
+const grid = document.quertySelection('.grid')
+ var.cardsChosen = []
+ var.cardsChosenId = []
+
+
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      var card = document.createElement('img')
+      card.setAttribute('scr', 'images/CardBack1.png')
+      card.setAttribute('data-id', i)
+
+      grid.appendChild(card)
+ 
+    
+    }}
+}
+
+function flipCard() {
+  var cardid = this.getAttribute('data-id')
+  cardChosen.push(cardArray[cardId].name)
+  cardsChosenid
+}
+
+createBoard()
