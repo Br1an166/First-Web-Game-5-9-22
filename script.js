3d1d9dae59a266c66bbb4394ea2a40c3cb5fcbaf
Brian1.66 <105819990+Br1an166@users.noreply.github.com>
2022-06-09 01:38:14 +0000

    a lot of progress

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
 document.addEventListener('DOMContentLoaded'), () => {
 
-const cardArray = [
-  {
-    name: 'Red',
-    img: 'Images/Red1.png'
-  },
-{
-    name: 'Blue',
-    img: 'Images/Blue1.png'
-  },
+  const cardArray = [
+    {
+      name: 'Red',
+      img: 'Images/Red1.png'
+    },
+    {
+      name: 'Blue',
+      img: 'Images/Blue1.png'
+    },
 
-{
-    name: 'Yellow',
-    img: 'Images/Yellow1.png'
-  },
+    {
+      name: 'Yellow',
+      img: 'Images/Yellow1.png'
+    },
 
-{
-    name: 'Green',
-    img: 'Images/Green1.png'
-  },
+    {
+      name: 'Green',
+      img: 'Images/Green1.png'
+    },
 
-{
-    name: 'Orange',
-    img: 'Images/Orange1.png'
-  },
+    {
+      name: 'Orange',
+      img: 'Images/Orange1.png'
+    },
 
-{
-    name: 'Purple',
-    img: 'Images/Purple1.png'
-  },
-]
+    {
+      name: 'Purple',
+      img: 'Images/Purple1.png'
+    },
+  ]
 
-const grid = document.quertySelection('.grid')
- var cardsChosen = []
- var cardsChosenId = []
+  const grid = document.quertySelection('.grid')
+  var cardsChosen = []
+  var cardsChosenId = []
+  var cardsWon = []
 
 
   function createBoard() {
@@ -41,24 +42,38 @@ const grid = document.quertySelection('.grid')
       var card = document.createElement('img')
       card.setAttribute('scr', 'images/CardBack1.png')
       card.setAttribute('data-id', i)
-
       grid.appendChild(card)
- 
-    
-    }}
+
+
+    }
+  }
 }
-function checkForMatchs () {
+function checkForMatchs() {
   var card = document.querSelectionAll('img')
   const optionOneId = cardsChosenId[0]
+  const optionTwoId = cardsChosenId[1]
+  if (cardsChosen[0] === cardsChosen[1]) {
+    alert('!!you found a match!!')
+    cards[opinionOneId].setAttribute('scr', 'image/Blank1.png')
+     cards[opinionTwoId].setAttribute('scr', 'image/Blank1.png')
+    cardsWon.push(cardsChosen)
+  } else {
+    cards[optionOneId].setAttribute('scr', images/CardBack1.jpeg)
+     cards[optionTwoId].setAttribute('scr', images/CardBack1.jpeg)
+    alert('D: Nope, try again D:')
+    
+  }
+
+
 }
 
 function flipCard() {
   var cardId = this.getAttribute('data-id')
   cardChosen.push(cardArray[cardId].name)
   cardsChosenId.push(cardArray[cardId].name)
-  this.setAtribute('scr',cardArray[cardId].img)
+  this.setAtribute('scr', cardArray[cardId].img)
   if (cardsChosen.legnth === 2) {
-    setTimeout(checkForMatch,500)
+    setTimeout(checkForMatch, 500)
   }
 }
 
